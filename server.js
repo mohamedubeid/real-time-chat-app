c7771bca5f39c7ff6cc79d5bc5d6c350b4062dbe
mohamedubeid <abomuin112@hotmail.com>
2022-07-31 09:03:23 +0300

    add dotenv file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const http = require('http');
@@ -65,7 +66,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
